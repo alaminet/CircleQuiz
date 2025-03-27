12738ba49efc833c451ac486bd3ce63e883b4c6b
Al Amin ET <alaminet.bd@gmail.com>
2025-03-27 21:58:54 +0600

    user-slice-initial-value-cng

--- frontend/src/lib/features/slice/userSlice.js
+++ frontend/src/lib/features/slice/userSlice.js
@@ -1,19 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-let initialState = {
-  login: null,
-};
-
-if (typeof window !== "undefined") {
-  const storedUser = localStorage.getItem("user");
-  initialState = {
-    login: storedUser ? JSON.parse(storedUser) : null,
-  };
-}
-
 export const userSlice = createSlice({
   name: "Login",
-  initialState,
+  initialState: {
+    login: null,
+  },
   reducers: {
     Loginuser: (state, action) => {
       state.login = action.payload;
