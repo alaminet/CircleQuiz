852486b978fc809ae4c9b8ad3f910ccf0205fcfd
Al Amin ET <alaminet.bd@gmail.com>
2025-04-04 15:19:30 +0600

    auth-trusthost-true

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -3,17 +3,18 @@ import Google from "next-auth/providers/google";
 import Facebook from "next-auth/providers/facebook";
 import LinkedIn from "next-auth/providers/linkedin";
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [Google, Facebook],
-    callbacks: {
-      async jwt({ token, account }) {
-        if (account) {
-          token.accessToken = account.access_token;
-        }
-        return token;
-      },
-      async session({ session, token }) {
-        session.accessToken = token.accessToken;
-        return session;
-      },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
     },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
